37e1f7ce9d9dcf674e952db1fb140150a3ac156d
Michael Johnson <michael.ben.johnson@gmail.com>
2020-10-19 17:00:10 +0100

    Updates

---
+++ precache-manifest.99315b2034a251bbb9f16ffec6479194.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df6b9f0969c36c0265b819e32cdd7bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e50111ff06b0c4377de6",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "3dfc5ff3df6716b35d97",
+    "url": "/static/css/main.c7e72702.chunk.css"
+  },
+  {
+    "revision": "e50111ff06b0c4377de6",
+    "url": "/static/js/2.be1b3b5e.chunk.js"
+  },
+  {
+    "revision": "d032987588c94c7fd80edbeebf10860a",
+    "url": "/static/js/2.be1b3b5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dfc5ff3df6716b35d97",
+    "url": "/static/js/main.647bfe17.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b0fd5793a7f366b6308749ce3386530d",
+    "url": "/static/media/adestra-logo.b0fd5793.png"
+  },
+  {
+    "revision": "e6cdd725bb728d337c9e4dd46fb0ae8d",
+    "url": "/static/media/background.e6cdd725.png"
+  },
+  {
+    "revision": "171c4627a92de36d5e6f75f09e1ec2f2",
+    "url": "/static/media/me.171c4627.jpg"
+  },
+  {
+    "revision": "a09ee1a02314c272cb9c31af7d0ce0a6",
+    "url": "/static/media/redeye-logo.a09ee1a0.png"
+  },
+  {
+    "revision": "7c58f65bb06b753a3cdf6a6bdb29f21c",
+    "url": "/static/media/reed-logo.7c58f65b.png"
+  },
+  {
+    "revision": "c33da03d299ddcc1185eeed2e17e1c6f",
+    "url": "/static/media/reed-logo.c33da03d.svg"
+  }
+]);
